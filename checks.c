e3cbbb1da515cdc63c24bad68c17eed8a2e67e9b
Eduard Zingerman <eddyz87@gmail.com>
2026-06-29 03:26:39 -0700

    wip: deduce_bounds_64_from_step

--- checks.c
+++ checks.c
@@ -9,6 +9,8 @@
 #include "checks.h"
 #undef T
 
+u16 nondet_u16(void);
+
 void check_32_from_64(void)
 {
 	struct cnum64 a = { nondet_u64(), nondet_u64() };
@@ -36,4 +38,45 @@ void check_64_32_intersect(void)
 	__CPROVER_assume(cnum64_contains(a, v) && cnum32_contains(b, (u32)v));
 
 	assert(cnum64_contains(c, v));
+}
+
+static struct cnum64 deduce_bounds_64_from_step(struct cnum64 in, u16 base, u16 step)
+{
+	u64 end = in.base + in.size;
+	u64 start = in.base;
+	u64 size = in.size;
+	u64 d;
+
+	d = (step - (start - base) % step) % step;
+	d = min(d, base - start);
+	start += d;
+	if (d > size)
+		return CNUM64_EMPTY;
+	size -= d;
+	d = (end - base) % step;
+	d = min(d, base - start);
+	end -= d;
+	if (d > size)
+		return CNUM64_EMPTY;
+
+	return (struct cnum64) {
+		.base = start,
+		.size = end - start,
+	};
+}
+
+void check_64_from_step(void)
+{
+	struct cnum64 a = { nondet_u64(), nondet_u64() };
+        u16 base = nondet_u16();
+	u16 step = nondet_u16();
+	u64 v = nondet_u64();
+
+	__CPROVER_assume(step > 0);
+	__CPROVER_assume(base < step);
+	__CPROVER_assume(cnum64_contains(a, v));
+	__CPROVER_assume((v - base) % step == 0);
+
+        struct cnum64 b = deduce_bounds_64_from_step(a, base, step);
+	assert(cnum64_contains(b, v));
 }
